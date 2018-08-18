a1455e6d70c55ec6aa202f225657784ff681095c
Supachai Chaimangua (Tor) <tortechnocom@gmail.com>
2018-08-18 10:01:54 +0700

    Update example to insert to mongo db

--- api/v1/example.go
+++ api/v1/example.go
@@ -2,13 +2,20 @@ package v1
 
 import (
 	"net/http"
-
+	"github.com/iot-bb/iot-bb-api/dao"
 	"github.com/gin-gonic/gin"
 )
+
+// M map
+type M map[string]interface{}
+var db = dao.DAO()
 
 // Example Api
 func Example(router *gin.RouterGroup) {
 	router.GET("/example", func (c *gin.Context) {
+		println("==> Example v1")
+		cc := db.C("Test")
+		cc.Insert(M{"api": "v1.0"})
 		c.JSON(http.StatusOK, gin.H{"message": "Example v1", "status": http.StatusOK})
 	})
 }
--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"github.com/iot-bb/iot-bb-api/api"
-	"github.com/iot-bb/iot-bb-api/dao"
 	// "github.com/iot-bb/iot-bb-api/models"
 )
-// M map
-type M map[string]interface{}
 
 func main() {
 	router := api.API()
-	db := dao.DAO()
-	c := db.C("Test")
-	c.Insert(M{"key": "value"})
-	print("==>", c.Create)
 	router.Run(":8080")
 }
